855e46733970bba4c360c281ef1d7475fed9d0c4
Дмитрий <bzlamshik@outlook.com>
2018-11-24 12:32:35 +0300

    Изменено имя формы

---
+++ ScreenFromMonitor/FMain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenFromMonitor
+{
+    public partial class FMain : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        public FMain()
+        {
+            InitializeComponent();
+            while (true)
+            {
+                System.Threading.Thread.Sleep(1000);
+                var s = DateTime.Now.ToString("mm-ss") + ".bmp";
+                ImageFromScreen().Save(s);
+
+            }
+        }
+        public Bitmap ImageFromScreen()
+        {
+            Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppRgb);
+            Graphics gr = Graphics.FromImage(bmp);
+            gr.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y, 0, 0, Screen.PrimaryScreen.Bounds.Size, CopyPixelOperation.SourceCopy);
+            return bmp;
+        }
+    }
+}
